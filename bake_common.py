91e18bffff51f8af48d0c39bc164c8f2a5db3e99
Deeheks <jeffredz@gmail.com>
2026-05-02 12:36:57 -0400

    Make sure to show mesh objects in viewport before baking. Fix #376

--- bake_common.py
+++ bake_common.py
@@ -283,6 +283,12 @@ def remember_before_bake(yp=None, mat=None):
         book['ori_col_hide_viewport'] = [c for c in bpy.data.collections if c.hide_viewport]
         book['ori_col_hide_render'] = [c for c in bpy.data.collections if c.hide_render]
 
+        # Remember space data
+        space = bpy.context.space_data
+        if space.type == 'VIEW_3D':
+            book['ori_space_show_object_viewport_mesh'] = space.show_object_viewport_mesh
+            book['ori_space_show_object_select_mesh'] = space.show_object_select_mesh
+
     else: 
         book['ori_hide_selects'] = [o for o in scene.objects if o.hide_select]
         book['ori_active_selected_objs'] = [o for o in scene.objects if o.select]
@@ -980,6 +986,13 @@ def recover_bake_settings(book, yp=None, recover_active_uv=False, mat=None):
             if o in book['ori_hide_selects']:
                 o.hide_select = True
             else: o.hide_select = False
+
+            # Recover space data
+            space = bpy.context.space_data
+            if space.type == 'VIEW_3D':
+                space.show_object_viewport_mesh = book['ori_space_show_object_viewport_mesh']
+                space.show_object_select_mesh = book['ori_space_show_object_select_mesh']
+
     else:
         for o in scene.objects:
             if o in book['ori_active_selected_objs']:
